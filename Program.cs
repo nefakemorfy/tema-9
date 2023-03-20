502e1457fdae49874915956fb1e3da81bde71023
Студент <@KAB31-14>
2023-03-20 08:55:45 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определить общую массу груза.
+            int[] weights = { 10, 20, 30, 40, 50 };
+            int n = weights.Length;
+            int totalWeight = 0;
+            for (int i = 0; i < n; i++)
+            {
+                totalWeight += weights[i];
+            }
+            Console.WriteLine("Общая масса груза: " + totalWeight);
+            Console.Read();
+        }
+    }
+}
